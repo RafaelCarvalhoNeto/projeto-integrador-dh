fe3bf9dcf3cc43c8363ff91a1fb1bde31d916b08
Rafael Carvalho <rafaelcarvalhoneto@gmail.com>
2020-05-22 17:09:03 -0300

    corrigindo footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,61 +1,61 @@
 
-    <footer class="container-fluid mt-5 mb-0 mx-0 pt-5 px-0">
+  <footer class="container-fluid mt-5 mb-0 mx-0 pt-5 px-0">
     <div id="footer">
-    <div class="container text-white bg-dark p-4">
-      <div class="row">
-        <div class="col-6 col-md-8 col-lg-7">
-          <div class="row text-center">
-            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
-              <ul class="list-unstyled">
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-              </ul>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
-              <ul class="list-unstyled">
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-              </ul>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
-              <ul class="list-unstyled">
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-                <li class="btn-link"> <a>Link </a> </li>
-              </ul>
+      <div class="container text-white bg-dark p-4">
+        <div class="row">
+          <div class="col-6 col-md-8 col-lg-7">
+            <div class="row text-center">
+              <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+                <ul class="list-unstyled">
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                </ul>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+                <ul class="list-unstyled">
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                </ul>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+                <ul class="list-unstyled">
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                  <li class="btn-link"> <a>Link </a> </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-4 col-lg-5 col-6">
-          <address>
-            <strong>Ecommerce, Inc.</strong><br>
-            Rua Alameda Santos 110<br>
-            Sao Paulo, SP, 9110-999<br>
-            <abbr title="Telephone">Tel:</abbr> (123) 456-7890
-          </address>
-          <address>
-            <strong>SAC</strong><br>
-            <a href="mailto:#">ecommerce@exemplo.com</a>
-          </address>
+          <div class="col-md-4 col-lg-5 col-6">
+            <address>
+              <strong>Ecommerce, Inc.</strong><br>
+              Rua Alameda Santos 110<br>
+              Sao Paulo, SP, 9110-999<br>
+              <abbr title="Telephone">Tel:</abbr> (123) 456-7890
+            </address>
+            <address>
+              <strong>SAC</strong><br>
+              <a href="mailto:#">ecommerce@exemplo.com</a>
+            </address>
+          </div>
         </div>
       </div>
+          <div class="d-flex flex-row flex-nowrap justify-content-center py-3 " id="footer">
+              <small>xxxx &copy; | CNPJ 01.012.012/0001-99 | <a href="#" title="Políticas e Termos" data-toggle="modal" data-target="#modalPoliticas">Políticas de Privacidade</a> | <a href="#" title="Trocas e Devoluções" data-toggle="modal" data-target="#modalTrocas">Trocas e Devoluções</a></small>
+          </div>
     </div>
-        <div class="d-flex flex-row flex-nowrap justify-content-center py-3 " id="footer">
-            <small>xxxx &copy; | CNPJ 01.012.012/0001-99 | <a href="#" title="Políticas e Termos" data-toggle="modal" data-target="#modalPoliticas">Políticas de Privacidade</a> | <a href="#" title="Trocas e Devoluções" data-toggle="modal" data-target="#modalTrocas">Trocas e Devoluções</a></small>
-        </div>
-        </div>
   </footer>
  
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </body>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </html>
