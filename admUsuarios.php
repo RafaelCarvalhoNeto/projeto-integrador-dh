54b9993cdbecd0da4013b1d37278210442f614c1
Rodrigo Morais Machado <rodrigmachado11@gmail.com>
2020-05-28 20:10:09 -0300

    criando tabela adm_usuários

---
+++ admUsuarios.php
@@ -0,0 +1,28 @@
+<?php require_once("./inc/head.php"); ?>
+<?php require_once("./inc/header.php"); ?>
+    <table class="table mt-5">
+        <thead class="thead-dark">
+            <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>CPF</th>
+            <th>Email</th>
+            <th>Senha</th>
+            <th>CEP</th>
+            <th>Cidade</th>
+            <th>UF</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>SDFHDFG</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <?php require_once("./inc/footer.php"); ?>
