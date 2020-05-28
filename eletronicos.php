a230405febb8135d1863d61061cecb8c9d643ac1
Rafael Carvalho <rafaelcarvalhoneto@gmail.com>
2020-05-27 22:05:26 -0300

    adicionando paginas

---
+++ eletronicos.php
@@ -0,0 +1,186 @@
+<?php $tituloDaPagina = "Eletrônicos" ?>
+<?php require_once("./inc/head.php"); ?>
+<?php require_once("./inc/header.php"); ?>
+
+<div class="container">
+    <h2 class="col-12 p-3 mt-3 mb-3 text-dark border-bottom"><?= $tituloDaPagina?></h2>
+
+    <div class="mb-3 col-lg-12 px-0">
+        <img class="d-block w-100" src= "./assets/img/eletronicos-poster.png" >
+    </div>
+
+    <div class="row text-center">
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\celular.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Samsung S20</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+
+    <div class="row text-center">
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\celular.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Samsung S20</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\computador.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Computador Zika</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\controle-ps5.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Controle PS5</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>       
+    </div>
+
+    <div class="row text-center mt-4">
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php" class="link">
+                <div class="card avancar">
+                <img class="card-img-top mt-3" src="assets\img\celular.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h3 class="card-title" id="produto">Samsung S20</h3>
+                    <p class="card-text" id="preco">R$ 900,00</p>
+                </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\computador.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Computador Zika</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\controle-ps5.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Controle PS5</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>       
+    </div>
+
+    <div class="row text-center mt-4">
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php" class="link">
+                <div class="card avancar">
+                <img class="card-img-top mt-3" src="assets\img\celular.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h3 class="card-title" id="produto">Samsung S20</h3>
+                    <p class="card-text" id="preco">R$ 900,00</p>
+                </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\computador.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Computador Zika</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\controle-ps5.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Controle PS5</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>       
+    </div>
+
+    <div class="row text-center mt-4">
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php" class="link">
+                <div class="card avancar">
+                <img class="card-img-top mt-3" src="assets\img\celular.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h3 class="card-title" id="produto">Samsung S20</h3>
+                    <p class="card-text" id="preco">R$ 900,00</p>
+                </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\computador.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Computador Zika</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4 pb-1 pb-md-0">
+            <a href="detalheProduto.php">
+                <div class="card avancar">
+                    <img class="card-img-top mt-3" src="assets\img\controle-ps5.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title" id="produto">Controle PS5</h3>
+                        <p class="card-text" id="preco">R$ 900,00</p>
+                    </div>
+                </div>
+            </a>
+        </div>       
+    </div>
+
+
+
+</div>
+
+
+
+
+
+<?php require_once("./inc/footer.php"); ?>
